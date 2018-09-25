258d6c0166cd95131744388bc16945c46e8eb7f5
new user <aljones15@gmail.com>
2018-09-25 09:30:02 +0000

    PlayerAvatar is up

--- src/components/GameOptions.vue
+++ src/components/GameOptions.vue
@@ -1,24 +1,31 @@
 <template>
   <vk-grid class="game-options" divided>
-     <div class="uk-width-1-4">
-       <vk-icon icon="user" />
+     <div class="uk-width-1-5">
+         <PlayerAvatar :type='player.type' v-for='player in players' />         
      </div>
-     <vk-grid class="uk-width-1-4">
-       <vk-icon icon="play-circle" /> <span>{{round}}</span>
-     </vk-grid>
+     <div class="uk-width-1-5">
+       <vk-icon icon="play-circle" class='float-left' /> <span class='float-right'>{{round}}</span>
+     </div>
+     <div class="uk-width-1-5">
+       <vk-icon icon="bolt" class='float-left' /> <span class='float-right'>Wins</span>
+     </div>
   </vk-grid>
 </template>
 
 <script>
-
 import { mapState } from 'vuex';
+import PlayerAvatar from './PlayerAvatar.vue';
 
 export default {
   name: 'GameOptions',
   computed: {
       ...mapState({
-          round: state => state.round
+          round: state => state.round,
+          players: state => state.players,
       }),
+  },
+  components: {
+      PlayerAvatar
   }
 }
 </script>
@@ -27,5 +34,11 @@ export default {
 <style scoped>
     .game-options {
         margin: 1rem;
+    }
+    .float-left {
+        float: left;
+    }
+    .float-right {
+        float: right;
     }
 </style>
---
+++ src/components/PlayerAvatar.vue
@@ -0,0 +1,24 @@
+<template>
+    <vk-icon icon="user" v-if="human" /> 
+    <RobotIcon v-else />
+</template>
+
+<script>
+import RobotIcon from './RobotIcon.vue';
+import playerTypes from '../constants/playerTypes';
+
+export default {
+  name: 'PlayerAvatar',
+  props: ['type'],
+  computed: {
+      human: function() {
+          if (!this.type) return false;
+          return this.type === playerTypes.HUMAN;
+      }
+  },
+  components: {
+      RobotIcon
+  }
+}
+
+</script>
