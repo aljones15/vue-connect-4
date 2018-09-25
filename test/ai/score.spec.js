05c5192653825030635c48a0f27eeeef4d65ed40
new user <aljones15@gmail.com>
2018-09-25 07:13:42 +0000

    finished tests for neighbors

--- test/ai/score.spec.js
+++ test/ai/score.spec.js
@@ -48,4 +48,49 @@ describe('score - withNeighbors', function() {
         expect(neighbors[1], 'Expected neighbors to include tile').to.include(tile);
         expect(neighbors[1][1], 'Expected tile to be the first in the middle').to.deep.eql(tile);
     });
+
+    it('should return a smaller set when on the top left edge', function() {
+        const tile = board[0][0];
+        const neighbors = withNeighbors(tile, board);
+        expect(neighbors).to.be.an('array');
+        expect(neighbors[0], 'Expected Top Row to be have 0').to.have.lengthOf(0);
+        expect(neighbors[1], 'Expected Middle Row to have 2').to.have.lengthOf(2);
+        expect(neighbors[2], 'Expected Bottom Row to have 2').to.have.lengthOf(2);
+        expect(neighbors[1], 'Expected neighbors to include tile').to.include(tile);
+        expect(neighbors[1][0], 'Expected tile to be the first in the middle').to.deep.eql(tile);
+    });
+
+    it('should return a smaller set when on the top right edge', function() {
+        const tile = board[0][6];
+        const neighbors = withNeighbors(tile, board);
+        expect(neighbors).to.be.an('array');
+        expect(neighbors[0], 'Expected Top Row to be have 0').to.have.lengthOf(0);
+        expect(neighbors[1], 'Expected Middle Row to have 2').to.have.lengthOf(2);
+        expect(neighbors[2], 'Expected Bottom Row to have 2').to.have.lengthOf(2);
+        expect(neighbors[1], 'Expected neighbors to include tile').to.include(tile);
+        expect(neighbors[1][1], 'Expected tile to be the first in the middle').to.deep.eql(tile);
+    });
+
+    it('should return a smaller set when on the bottom right edge', function() {
+        const tile = board[5][6];
+        const neighbors = withNeighbors(tile, board);
+        expect(neighbors).to.be.an('array');
+        expect(neighbors[0], 'Expected Top Row to be have 2').to.have.lengthOf(2);
+        expect(neighbors[1], 'Expected Middle Row to have 2').to.have.lengthOf(2);
+        expect(neighbors[2], 'Expected Bottom Row to have 0').to.have.lengthOf(0);
+        expect(neighbors[1], 'Expected neighbors to include tile').to.include(tile);
+        expect(neighbors[1][1], 'Expected tile to be the first in the middle').to.deep.eql(tile);
+    });
+
+    it('should return a smaller set when on the bottom left edge', function() {
+        const tile = board[5][0];
+        const neighbors = withNeighbors(tile, board);
+        expect(neighbors).to.be.an('array');
+        expect(neighbors[0], 'Expected Top Row to be have 2').to.have.lengthOf(2);
+        expect(neighbors[1], 'Expected Middle Row to have 2').to.have.lengthOf(2);
+        expect(neighbors[2], 'Expected Bottom Row to have 0').to.have.lengthOf(0);
+        expect(neighbors[1], 'Expected neighbors to include tile').to.include(tile);
+        expect(neighbors[1][0], 'Expected tile to be the first in the middle').to.deep.eql(tile);
+    });
+
 });
