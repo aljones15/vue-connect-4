740416989dd6323a2e25ece2d9b887ea8c877a0b
andrewjones <aljones15@gmail.com>
2018-09-26 14:12:49 +0800

    removed inherit classes

--- src/main.js
+++ src/main.js
@@ -4,14 +4,12 @@ import Vuikit from 'vuikit'
 import VuikitIcons from '@vuikit/icons'
 import Vuex from 'vuex';
 import makeStore from './state';
-import inheritClasses from 'v-inheritclasses'
 
 import '@vuikit/theme'
 
 Vue.use(Vuikit)
 Vue.use(VuikitIcons)
 Vue.use(Vuex);
-Vue.use(inheritClasses);
 Vue.config.productionTip = false
 
 export const store = makeStore();
