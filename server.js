38900c2564001f923d9ee6898e7e9c56ccb4ce4b
andrewjones <aljones15@gmail.com>
2018-09-28 21:48:14 +0800

    heroku port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.NODE_ENV === 'production' ? 3080 : 3000;
+const port = process.env.PORT || 3080;
 const app = express()
     app.use('/', express.static(path.join(__dirname, 'dist')));
 
