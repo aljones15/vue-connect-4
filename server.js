c6e8daba1292922a8f4df695a9dfd4f9f6643ebc
andrewjones <aljones15@gmail.com>
2018-09-28 21:55:21 +0800

    further herok-isms

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 3080;
 const app = express()
-    app.use('/', express.static(path.join(__dirname, 'dist')));
+    app.use(express.static(path.join(__dirname, 'dist')));
 
         app.listen(port, () => {
             console.log('connect 4 is listening on ' + port); // eslint-disable-line no-console
