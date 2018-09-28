6ca9ca024a0b46cf5202b3e62113e122bc096691
andrewjones <aljones15@gmail.com>
2018-09-29 00:06:28 +0800

    joined

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const path = require('path');
 const port = process.env.PORT || 3080;
 const app = express()
 
-  app.use(express.static(path.join(__dirname, 'public', 'dist')));
-
+  app.use(express.static(path.join(__dirname, 'dist')));
   app.listen(port, () => {
-     console.log('connect 4 is listening on ' + port); // eslint-disable-line no-console
+     console.log('connect 4 is listening on ' + port + ' dir name is ' + __dirname); // eslint-disable-line no-console
   });
